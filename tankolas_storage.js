a2bd583f6d23d438e0c18bc225927c48dca04426
Kámán Marcell <kaman.marcell.2009.10.28@gmail.com>
2026-09-17 09:20:55 +0200

    more tankolás

--- tankolas_storage.js
+++ tankolas_storage.js
@@ -11,7 +11,26 @@ function storeData(date, amount, cost) {
 
 
 
-
+function listing(){
+    let data=getData()
+    const list=document.getElementById("tankolasList")
+    const datemax=document.getElementById("tankolasFiltermax")
+    const datemin=document.getElementById("tankolasFiltermin")
+    if (datemin.value!=""){
+        data=data.filter((tankolas)=>new Date(tankolas["date"])>= new Date(datemin.value))
+    }
+    if (datemax.value!=""){
+        data=data.filter((tankolas)=>new Date(tankolas["date"])<= new Date(datemax.value))
+    }
+    list.innerHTML=""
+    for (item in data){
+        list.innerHTML+=`<tr>
+					<td class="w-1/3 text-center">${item["date"]}</td>
+					<td class="w-1/3 text-center">${item["amount"]}</td>
+					<td class="w-1/3 text-center">${item["cost"]}</td>
+				</tr>`
+    }
+}
 
 
 
